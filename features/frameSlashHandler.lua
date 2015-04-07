9791834319c66f0a1a60ee324ac57f6c628ba301
andy <andy@andydote.co.uk>
2015-04-07 23:10:41 +0100

    frameDebug: use dark.slash

--- features/frameSlashHandler.lua
+++ features/frameSlashHandler.lua
@@ -2,57 +2,57 @@ local addon, ns = ...
 
 local frameDebugSlashHandler = function()
 
-	local onSlash = function(frameName)
+	local rl = ns.lib.slash:new("frame", {
 
-		local frame
+		_default = function(self, frameName)
 
-		if frameName and frameName ~= "" then
-			frame = _G[frameName]
-		else
-			frame = GetMouseFocus()
+			local frame
+
+			if frameName and frameName ~= "" then
+				frame = _G[frameName]
+			else
+				frame = GetMouseFocus()
+			end
+
+			if frame == nil or frame:GetName() == nil then
+				print("Invalid frame name.")
+				return
+			end
+
+			local point, relativeTo, relativePoint, xOfs, yOfs = frame:GetPoint()
+
+			print("|cffCC0000~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+			print("Name: |cffFFD100"..frame:GetName())
+
+			local parent = frame:GetParent()
+
+			if parent ~= nil then
+				parent = parent:GetName() or tostring(parent)
+			end
+
+			print("Parent: |cffFFD100".. (parent or "nil"))
+
+			print("Width: |cffFFD100"..format("%.2f",frame:GetWidth()))
+			print("Height: |cffFFD100"..format("%.2f",frame:GetHeight()))
+			print("Strata: |cffFFD100"..frame:GetFrameStrata())
+			print("Level: |cffFFD100"..frame:GetFrameLevel())
+
+			if xOfs then
+				print("X: |cffFFD100"..format("%.2f",xOfs))
+			end
+
+			if yOfs then
+				print("Y: |cffFFD100"..format("%.2f",yOfs))
+			end
+
+			if relativeTo then
+				print("Point: |cffFFD100"..point.."|r anchored to ".. (relativeTo:GetName() or tostring(relativeTo)) .."'s |cffFFD100"..relativePoint)
+			end
+
+			print("|cffCC0000~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
 		end
-
-		if frame == nil or frame:GetName() == nil then
-			print("Invalid frame name.")
-			return
-		end
-
-		local point, relativeTo, relativePoint, xOfs, yOfs = frame:GetPoint()
-
-		print("|cffCC0000~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-		print("Name: |cffFFD100"..frame:GetName())
-
-		local parent = frame:GetParent()
-
-		if parent ~= nil then
-			parent = parent:GetName() or tostring(parent)
-		end
-
-		print("Parent: |cffFFD100".. (parent or "nil"))
-
-		print("Width: |cffFFD100"..format("%.2f",frame:GetWidth()))
-		print("Height: |cffFFD100"..format("%.2f",frame:GetHeight()))
-		print("Strata: |cffFFD100"..frame:GetFrameStrata())
-		print("Level: |cffFFD100"..frame:GetFrameLevel())
-
-		if xOfs then
-			print("X: |cffFFD100"..format("%.2f",xOfs))
-		end
-
-		if yOfs then
-			print("Y: |cffFFD100"..format("%.2f",yOfs))
-		end
-
-		if relativeTo then
-			print("Point: |cffFFD100"..point.."|r anchored to ".. (relativeTo:GetName() or tostring(relativeTo)) .."'s |cffFFD100"..relativePoint)
-		end
-
-		print("|cffCC0000~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-
-	end
-
-	SLASH_FRAME1 = "/frame"
-	SlashCmdList["FRAME"] = onSlash
+	})
 
 end
 
